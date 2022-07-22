06d07903ad67e3c2790c186f9ff96178e1e18b26
Renzo Silva <renzoasilva@gmail.com>
2022-07-22 15:24:18 -0400

    New ways to add, update, delete, set.

--- index.js
+++ index.js
@@ -18,5 +18,38 @@ const db = getFirestore()
 //   .catch(err => console.error(err))
   //console.log()
 
-db.collection ('cars').doc('lambo')
-  .set({ make: 'Lamborghini', model: 'Diablo', year: 2020, color: 'yellow'})
+// db.collection ('cars').doc('lambo')
+//   .set({ make: 'Lamborghini', model: 'Diablo', year: 2020, color: 'yellow'})
+
+// db.collection('cars').doc('lambo')
+//   .update({ ac: "yes", color: 'hot pink' })
+
+//Get a single document:
+// db.collection('cars').doc('lambo').get()
+//   .then(doc => {
+//     console.log(doc.id)
+//     console.log(doc.data())
+//   })
+//   .catch(console.log.error)
+
+// Get a whole collection:
+db.collection('cars').get()
+  .then(collection => {
+    collection.docs.forEach(doc => console.log(doc.id, doc.data()))
+  })
+  .catch(console.error)
+
+  //Querry docs from collection:
+  db.collection('cars')
+    .where('year', '>=', 2015)
+    .get()
+      .then(collection => {
+        const cars = collection.docs.map(doc => {
+            let car = doc.data() // {make, model, color, year}
+            car.id = doc.id      // {make, model, color, year, id}
+            return car
+        })
+        console.log(cars)
+      })
+      .catch(console.error)
+//.where('color')
