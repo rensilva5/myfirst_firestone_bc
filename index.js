021b575f27b36c58beee3a2fa1a8f0e23c980668
rensilva5 <renzoasilva@gmail.com>
2022-08-15 22:09:00 -0400

    more cars were added to the collection as practice

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ import { initializeApp, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import { credentials } from './credentials.js'
 
-initializeApp( {
-    credential: cert(credentials)
-})
+initializeApp( { credential: cert(credentials) })
 
 const db = getFirestore()
 
-// const car3 = ({ make: 'Audi', model: 'A3', year: 2018, color: 'grey'})
+//--// Adding one Car (car4)
 
+// const car4 =({ make: 'mercedez', model: 'i34', year: 2008, color: 'blue'})
+// db.collection('cars')
+//     .add(car4)
+//     .then(doc => {
+//     console.log('doc added:', doc.id)
+//     })
+//     .catch(err => console.error(err))
+//       console.log()
+
+// //--// Adding another car (car5)
+
+// const car5 = ({ make: 'chevy', model: 'cruze', year: 2017, color: 'silver'})
+// db.collection('cars')
+//     .add(car5)
+//     .then(doc => console.log('one more doc added', doc.id))
+//     .catch(err => console.error(err))
+//     console.log()
+
+//--// car has been added (car3)
+
+// const car3 = ({ make: 'Audi', model: 'A3', year: 2018, color: 'grey'})
 // db.collection('cars')
 //   .add(car3)
 //   .then (doc => {
@@ -18,13 +37,21 @@ const db = getFirestore()
 //   .catch(err => console.error(err))
 //   console.log()
 
+//--// making changes in one of the cars
+
 // db.collection ('cars').doc('lambo')
 //   .set({ make: 'Lamborghini', model: 'Diablo', year: 2020, color: 'yellow'})
+
+//--// Another car added
+
+// db.collection('cars').doc('Pirlo')
+//   .set({ make: 'ferrari', model: 'ultimate', year: 2020})
 
 // db.collection('cars').doc('lambo')
 //   .update({ ac: "yes", color: 'hot pink' })
 
-//Get a single document:
+//--// Getting a single document:
+
 // db.collection('cars').doc('lambo').get()
 //   .then(doc => {
 //     console.log(doc.id)
@@ -33,22 +60,26 @@ const db = getFirestore()
 //   .catch(console.log.error)
 
 // Get a whole collection:
-db.collection('cars').get()
-  .then(collection => {
-    collection.docs.forEach(doc => console.log(doc.id, doc.data()))
-  })
-  .catch(console.error)
 
+// db.collection('cars').get()
+//   .then(collection => {
+//     collection.docs.forEach(doc => console.log(doc.id, doc.data()))
+//   })
+//   .catch(console.error)
+
+  db.collection('cars').get()
+  .then(collection => collection.docs.forEach(doc => console.log(doc.id, doc.data())))
+  .catch(console.error)
   //Querry docs from collection:
-  db.collection('cars')
-    .where('year', '>=', 2015)
-    .get()
-      .then(collection => {
-        const cars = collection.docs.map(doc => {
-            let car = doc.data() // {make, model, color, year}
-            car.id = doc.id      // {make, model, color, year, id}
-            return car
-        })
-        console.log(cars)
-      })
-      .catch(console.error)
+  // db.collection('cars')
+  //   .where('year', '>=', 2015)
+  //   .get()
+  //     .then(collection => {
+  //       const cars = collection.docs.map(doc => {
+  //           let car = doc.data() // {make, model, color, year}
+  //           car.id = doc.id      // {make, model, color, year, id}
+  //           return car
+  //       })
+  //       console.log(cars)
+  //     })
+  //     .catch(console.error)
